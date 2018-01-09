2f0e16a4506a2f9ee3a7aff4639b225a3b970a1b
bryn.lloyd <bryn.lloyd@metoffice.gov.uk>
2018-01-09 09:58:11 +0000

    Requiring your own files

--- notes-nodes/app.js
+++ notes-nodes/app.js
@@ -1,8 +1,18 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+// console.log(_.isString(true));
+// console.log(_.isString('Bryn'));
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`)
+var filteredArray = _.uniq(['Bryn', 1, 'Bryn', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+//console.log('Result: ' + notes.add(9, -2));
+
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`)
