2ead240a9fd9e003cb30c1a0031feedca0cc5d4b
bryn.lloyd <bryn.lloyd@metoffice.gov.uk>
2018-01-16 16:51:31 +0000

    add .gitignore for node.js

--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -1,27 +1,33 @@
 const request = require('request');
+const customApiKeys = require('../customApiKeys');
 
 var geocodeAddress = (address, callback) => {
 
-  var encodedAddress = encodeURIComponent(address);
-  var apiKey = 'YOUR_API_KEY_GOSE_HERE'
-  var basePath = 'https://maps.googleapis.com/maps/api/geocode/json'
+  customApiKeys.getApiKeys((keys) => {
+    var apiKey = keys.googleMapsApiKey;
+    console.log(apiKey);
 
-  request({
-    url: `${basePath}?address=${encodedAddress}&key=${apiKey}`,
-    json: true
-  }, (error, response, body) => {
-    if (error){
-      callback('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-      callback('Unable to find that address.');
-    } else if(body.status === 'OK'){
-      callback(undefined, {
-        address: body.results[0].formatted_address,
-        latitude: body.results[0].geometry.location.lat,
-        longitude: body.results[0].geometry.location.lng
-      });
-    }
+    var encodedAddress = encodeURIComponent(address);
+    var basePath = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+    request({
+      url: `${basePath}?address=${encodedAddress}&key=${apiKey}`,
+      json: true
+    }, (error, response, body) => {
+      if (error){
+        callback('Unable to connect to Google servers.');
+      } else if (body.status === 'ZERO_RESULTS') {
+        callback('Unable to find that address.');
+      } else if(body.status === 'OK'){
+        callback(undefined, {
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng
+        });
+      }
+    });
   });
+
 };
 
 module.exports.geocodeAddress = geocodeAddress;
--- weather-app/weather/weather.js
+++ weather-app/weather/weather.js
@@ -1,23 +1,29 @@
 const request = require('request');
+const customApiKeys = require('../customApiKeys');
 
 var getWeather = (lat, lng, callback) => {
 
-  var apiKey = 'YOUR_API_KEY_GOSE_HERE'
-  var basePath = 'https://api.darksky.net/forecast/'
+  customApiKeys.getApiKeys((keys) => {
+    var apiKey = keys.darkSkyApiKey;
+    console.log(apiKey);
 
-  request({
-    url: `${basePath}${apiKey}/${lat},${lng}`,
-    json: true
-  }, (error, response, body) => {
-    if(!error && response.statusCode === 200){
-      callback(undefined, {
-        temperature: body.currently.temperature,
-        apparentTemperature: body.currently.apparentTemperature
-      });
-    } else {
-      callback('Unable to fetch weather.');
-    }
+    var basePath = 'https://api.darksky.net/forecast/'
+
+    request({
+      url: `${basePath}${apiKey}/${lat},${lng}`,
+      json: true
+    }, (error, response, body) => {
+      if(!error && response.statusCode === 200){
+        callback(undefined, {
+          temperature: body.currently.temperature,
+          apparentTemperature: body.currently.apparentTemperature
+        });
+      } else {
+        callback('Unable to fetch weather.');
+      }
+    });
   });
+
 };
 
 module.exports.getWeather = getWeather;
